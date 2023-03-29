41057f76a05550ef0f2a838e15fb6d0b08e82cdb
Giovanna Carvalho <100374203+ProjetosCarvalho@users.noreply.github.com>
2023-03-29 14:57:23 -0300

    Método Construtor

---
+++ metodo_construtor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace metodo_construtor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Produto p1 = new Produto(); (construtor 1)
+        //Produto p1 = new Produto("SACI PERERE"); (construtor 2)
+        
+        //(construtor 3)
+        Produto p1 = new Produto("ABC",10);
+
+        
+
+        p1.MostraDados();
+    }
+}
