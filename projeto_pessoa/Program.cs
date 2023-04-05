5aef4e2149f488ec33a3e7126fda6df2d3f4c86c
Giovanna Carvalho <100374203+ProjetosCarvalho@users.noreply.github.com>
2023-04-05 14:13:04 -0300

    Aula 6 - Métodos Construtores

---
+++ projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+       //Pessoa p1 = new Pessoa();
+       Pessoa p1 = new Pessoa("Giovanna",16);
+       //p1.insereNome("Giovanna");
+       //p1.insereSobrenome("Carvalho");
+       //p1.insereIdade(16);
+       p1.mostraDados();
+    }
+}
